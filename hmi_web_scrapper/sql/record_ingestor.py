59d21116ffc375a200b562a7c1b9baa09a628605
FelixSnz <felixsanchez0124@gmail.com>
2023-06-02 11:56:05 -0500

    logger added

--- hmi_web_scrapper/sql/record_ingestor.py
+++ hmi_web_scrapper/sql/record_ingestor.py
@@ -2,44 +2,49 @@ import os
 import pandas as pd
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
-from manager import DBManager
+from sql.db_manager import DBManager
 import configparser
+
+from sql.config import host, database, user, password
+
+from utils.logger import get_logger
 
 class CsvFileHandler(FileSystemEventHandler):
     def __init__(self, db_manager):
-        self.db_manager = db_manager
+        self.db_manager:DBManager = db_manager
+        self.logger = get_logger(__name__, self.__class__.__name__)
 
     def on_created(self, event):
-        if event.is_directory or not event.src_path.endswith('.csv'):
-            return
+        if event.is_directory:
+            self.logger.warn(f"Directory created: {event.src_path}")
+        if not event.src_path.endswith('.csv'):
+            self.logger.warn(f"Unexpected file type: {event.src_path}")
 
         df = pd.read_csv(event.src_path)
 
         if "E_Standard" not in df.columns:
-            print(f"Error: 'E_Standard' column not found in file {event.src_path}")
+            
+            self.logger.error(f"Error: 'E_Standard' column not found in file {event.src_path}")
             return
 
         for _, row in df.iterrows():
             table = row['E_Standard']
             data = row.drop('E_Standard').tolist()
             self.db_manager.insert(table, data)
+            self.logger.info(f"data: {data} inserted at {host}:{database}:{table}")
 
 
 class RecordIngestor:
     def __init__(self, path):
         self.path = path
-        self.db_manager = None
-        # Read database configuration
-        config = configparser.ConfigParser()
-        config.read('config.ini')
 
-        host = config['sql']['host']
-        database = config['sql']['database']
-        user = config['sql']['user']
-        password = config['sql']['password']
-
-        self.db_manager = DBManager(driver='your-driver', server=host,
-                                    database=database, user=user, password=password)
+        self.db_manager = DBManager(
+            server=host,
+            database=database,
+            user=user,
+            password=password
+        )
+        
         self.db_manager.connect()
         
 
