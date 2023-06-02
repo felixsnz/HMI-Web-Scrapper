3704d07618cf10c4b89d0c522e1c77c5ad06909a
FelixSnz <felixsanchez0124@gmail.com>
2023-06-02 11:55:02 -0500

    web scrapper improved

--- hmi_web_scrapper/web/driver.py
+++ hmi_web_scrapper/web/driver.py
@@ -1,42 +1,68 @@
-from typing import List
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.remote.webelement import WebElement
-from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.common.exceptions import WebDriverException
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
+from selenium import webdriver
+from selenium.common.exceptions import WebDriverException, NoSuchElementException, TimeoutException
+from utils.logger import get_logger
 
 
 class ChromeDriver(webdriver.Chrome):
 
     def __init__(self, driver_path):
+        self.logger = get_logger(__name__)
         try:
             options = Options()
             options.add_argument("--headless")
             service = Service(driver_path)
 
-            # Replace 'chromedriver' with the path to your ChromeDriver executable
             super().__init__(service=service, options=options)
             self.implicitly_wait(10)
+            self.wait = WebDriverWait(self, 10)
         except WebDriverException as e:
-            print(f"Error initializing WebDriver. {e}")
+            self.logger.error(f"Error initializing WebDriver. {e}")
 
     def navigate(self, url):
         try:
+            self.logger.info(f"navigating to {url}")
             self.get(url)
         except WebDriverException as e:
-            print(f"Error navigating to {url}. {e}")
+            self.logger.error(f"Error navigating to {url}. {e}")
 
-    def find_element(self, by, value, timeout=10):
+    def find_element(self, by, value):
         try:
-            element = WebDriverWait(self, timeout).until(
-                EC.presence_of_element_located((by, value))
-            )
-            return element
-        except WebDriverException as e:
-            print(f"Error finding element. {e}")
+            return super().find_element(by, value)
+        except NoSuchElementException:
+            self.logger.error(f"Error finding element. {by}, {value}")
+            return None
+
+    def wait_and_get_element(self, by, value):
+        try:
+            self.wait.until(EC.visibility_of_element_located((by, value)))
+            return self.find_element(by, value)
+        except TimeoutException:
+            self.logger.error(f"Timeout waiting for element. {by}, {value}")
+            return None
+
+    def find_elements(self, by, value):
+        try:
+            return super().find_elements(by, value)
+        except NoSuchElementException:
+            self.logger.error(f"Error finding elements. {by}, {value}")
+            return []
+
+    def wait_and_get_elements(self, by, value):
+        try:
+            self.wait.until(EC.visibility_of_all_elements_located((by, value)))
+            return self.find_elements(by, value)
+        except TimeoutException:
+            self.logger.error(f"Timeout waiting for elements. {by}, {value}")
+            return []
 
     def close(self):
-        self.quit()
+        try:
+            self.logger.info("Closing driver")
+            self.quit()
+        except WebDriverException as e:
+            self.logger.error(f"Error closing the driver. {e}")
--- hmi_web_scrapper/web/scrapper.py
+++ hmi_web_scrapper/web/scrapper.py
@@ -1,52 +1,49 @@
-from driver import ChromeDriver
+from web.driver import ChromeDriver
 from typing import List
-from selenium.webdriver.chrome.options import Options
-from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.remote.webelement import WebElement
 import requests
 import time
 import os
+from utils.logger import get_logger
 
 
 class Scrapper:
 
-    def __init__(self, url, driver:webdriver.Chrome) -> None:
+    def __init__(self, url, driver: ChromeDriver) -> None:
         self.url = url
         self.driver = driver
+        self.logger = get_logger(__name__)
 
     def run(self):
-        self.driver.get(self.url)
-
+        self.driver.navigate(self.url)
         downloaded_files = set()
+        self.logger.info("running scrapper...")
         try:
             while True:
                 try:
+                    
+                    # Navigate or refresh the page
+                    self.driver.navigate(self.url)
+                    self.driver.refresh()
+
                     csv_files = self.get_csv_files()
                     for file_name, file_url in csv_files:
+                        print(file_name)
                         if file_name not in downloaded_files:
-                            print(f"New CSV file: {file_name}: {file_url}")
+                            self.logger.info(f"New CSV file: {file_name}: {file_url}")
                             self.download_csv_file(file_name, file_url)
                             downloaded_files.add(file_name)
                     time.sleep(5)
-                except:
+                except Exception as e:
+                    self.logger.error(f"An error occurred in run method: {e}")
                     continue
         finally:
-            self.driver.quit()
-
+            self.driver.close()
 
     def get_csv_files(self):
-        
-
-        # Wait for the table to load
-        WebDriverWait(self.driver, 10).until(
-            EC.presence_of_element_located((By.TAG_NAME, 'table'))
-        )
-
-        # Get the table
-        table = self.driver.find_element(By.TAG_NAME, 'table')
+        # Wait for the table to load and get the table
+        table: WebElement = self.driver.wait_and_get_element(By.TAG_NAME, 'table')
 
         # Get all rows in the table
         table_rows = table.find_elements(By.TAG_NAME, 'tr')
@@ -54,7 +51,7 @@ class Scrapper:
         # Extract CSV file links from the table
         csv_files = []
         for row in table_rows[1:]:  # Start from the second row to skip the header row
-            cells:List[WebElement] = row.find_elements(By.TAG_NAME, 'td')
+            cells: List[WebElement] = row.find_elements(By.TAG_NAME, 'td')
             if cells:
                 link = cells[0].find_element(By.TAG_NAME, 'a')
 
@@ -71,7 +68,6 @@ class Scrapper:
             os.makedirs(download_path, exist_ok=True)
             with open(os.path.join(download_path, file_name), 'wb') as f:
                 f.write(response.content)
-            print(f"Downloaded {file_name} to {download_path}")
+            self.logger.info(f"Downloaded {file_name} to {download_path}")
         else:
-            print(f"Failed to download {file_name}: status code {response.status_code}")
-
+            self.logger.error(f"Failed to download {file_name}: status code {response.status_code}")
