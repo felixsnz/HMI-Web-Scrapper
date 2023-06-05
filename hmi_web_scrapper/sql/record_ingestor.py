bb5cb0f079961989cda27df68e12fd112b4f63f2
FelixSnz <felixsanchez0124@gmail.com>
2023-06-05 14:44:48 -0500

    try catch on_created handler

--- hmi_web_scrapper/sql/record_ingestor.py
+++ hmi_web_scrapper/sql/record_ingestor.py
@@ -3,7 +3,6 @@ import pandas as pd
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
 from sql.db_manager import DBManager
-import configparser
 
 from sql.config import host, database, user, password
 
@@ -17,21 +16,29 @@ class CsvFileHandler(FileSystemEventHandler):
     def on_created(self, event):
         if event.is_directory:
             self.logger.warn(f"Directory created: {event.src_path}")
-        if not event.src_path.endswith('.csv'):
+        if not event.src_path.lower().endswith('.csv'):
             self.logger.warn(f"Unexpected file type: {event.src_path}")
 
-        df = pd.read_csv(event.src_path)
-
-        if "E_Standard" not in df.columns:
+        try:
+            df = pd.read_csv(event.src_path)
+        except Exception as e:
+            self.logger.error(f"Couldn't read new file: '{event.src_path}'")
             
+        if "E_Standard" not in df.columns:
             self.logger.error(f"Error: 'E_Standard' column not found in file {event.src_path}")
             return
 
         for _, row in df.iterrows():
-            table = row['E_Standard']
-            data = row.drop('E_Standard').tolist()
-            self.db_manager.insert(table, data)
-            self.logger.info(f"data: {data} inserted at {host}:{database}:{table}")
+            try:
+                table = row['E_Standard']
+                data = row.drop('E_Standard').tolist()
+                self.db_manager.insert(table, data)
+            except Exception as e:
+                self.logger.error(f"Error while inserting csv data: {e}")
+            finally:
+                self.logger.debug(f"data: {data} inserted at {host}:{database}:{table}")
+                self.logger.debug(f"{self.db_manager.__get_column_names(table)}")
+
 
 
 class RecordIngestor:
