95b911118a452d46f22d3e4a1a1838e22f09d1cd
FelixSnz <felixsanchez0124@gmail.com>
2023-06-05 14:42:48 -0500

    main threads added

--- hmi_web_scrapper/main.py
+++ hmi_web_scrapper/main.py
@@ -15,10 +15,8 @@ hmi_ip_address = config['hmi']['ip']
 url = f'http://{hmi_ip_address}/logs/Customs/'
 
 def main():
-    
     run_bash_script("hmi_web_scrapper/scripts/kill_port_process.sh")
-    
-    
+
     # create an instance of ModbusServer
     modbus_server = ModbusServer()
 
@@ -26,21 +24,18 @@ def main():
     modbus_thread = threading.Thread(target=modbus_server.run)
     modbus_thread.start()
     
-    
     adpt_monitor = InterfaceMonitor('eth0')
     thread = threading.Thread(target=adpt_monitor.run)
     thread.start()
     
-    
-    
     # create an instance of FolderWatchdog
-    # ingestor = RecordIngestor(r"downloads")
-    # ingestor_thread = threading.Thread(target=ingestor.start)
-    # ingestor_thread.start()
+    ingestor = RecordIngestor(r"downloads")
+    ingestor_thread = threading.Thread(target=ingestor.start)
+    ingestor_thread.start()
 
-    # driver = ChromeDriver('/usr/lib/chromium-browser/chromedriver')
-    # scrapper = Scrapper(url, driver=driver)
-    # scrapper.run()
+    driver = ChromeDriver('/usr/lib/chromium-browser/chromedriver')
+    scrapper = Scrapper(url, driver=driver)
+    scrapper.run()
 
 if __name__ == '__main__':
     main()
