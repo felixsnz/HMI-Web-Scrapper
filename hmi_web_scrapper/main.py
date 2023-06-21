1a6def9e624b3b02e10657b93db2cf66e2b18f7a
FelixSnz <felixsanchez0124@gmail.com>
2023-06-21 11:02:42 -0500

    main with basic threads

--- hmi_web_scrapper/main.py
+++ hmi_web_scrapper/main.py
@@ -1,8 +1,9 @@
 from optiview_db.record_ingestor import RecordIngestor
-from raspberry.modbus import ModbusServer
+from optiview_db.host_ping_monitor import HostPingMonitor
+from raspberry.modbus import ModbusServer, ModbusClient
 from raspberry.ethernet import InterfaceMonitor
 
-from scrapper.event_handlers import EStandardLogEventHandler, RecordLogEventHandler
+from scrapper.event_handlers import RecordLogEventHandler
 from scrapper.watcher import LogWatcher
 from utils.logger import get_logger
 from utils.threads import StoppableThread
@@ -14,8 +15,9 @@ import threading
 import configparser
 
 config = configparser.ConfigParser()
-config.read('config.ini')
+config.read('/home/felix/projects/hmi_web_scrapper/config.ini')
 hmi_ip_address = config['hmi']['ip']
+sql_server_hostname = config['sql']['host']
 
 logger = get_logger(__name__)
 
@@ -23,16 +25,19 @@ def main():
     current_file_path = os.path.realpath(__file__)
     parent_directory_path = os.path.dirname(os.path.dirname(current_file_path))
     sg.app_path = parent_directory_path
-    #run_bash_script("hmi_web_scrapper/scripts/kill_port_process.sh")
-
-    #create an instance of ModbusServer
-    # modbus_server = ModbusServer()
-    # modbus_thread = threading.Thread(target=modbus_server.run)
-    # modbus_thread.start()
     
-    # adpt_monitor = InterfaceMonitor('eth0')
-    # thread = threading.Thread(target=adpt_monitor.run)
-    # thread.start()
+    def handle_connection_loss():
+        modbus_client = ModbusClient()
+        modbus_client.write_coil(1, 0)
+    
+    #create an instance of ModbusServer
+    modbus_server = ModbusServer()
+    modbus_thread = threading.Thread(target=modbus_server.run)
+    modbus_thread.start()
+    
+    time.sleep(5)
+    
+    #monitor = HostPingMonitor(sql_server_hostname, handle_connection_loss)
     
     downloads_path = os.path.join(parent_directory_path, r"downloads")
     print(downloads_path)
@@ -40,10 +45,11 @@ def main():
     ingestor = RecordIngestor(downloads_path)
     ingestor_thread = threading.Thread(target=ingestor.start)
     ingestor_thread.start()
-
-    #logs_watcher = LogWatcher(RecordLogEventHandler())
-    #logs_watcher_thread = threading.Thread(target=logs_watcher.run)
-    #logs_watcher_thread.start()
+    
+    time.sleep(5)
+    logs_watcher = LogWatcher(RecordLogEventHandler())
+    logs_watcher_thread = threading.Thread(target=logs_watcher.run)
+    logs_watcher_thread.start()
 
 if __name__ == '__main__':
     main()
